a532258c467d4cd61b45f78f15390f0181f0599f
luismo <luismmorandit@gmail.com>
2023-10-22 01:50:54 -0300

    mi primer go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/luismorandi/my_first_go
+
+go 1.21.3
+
+require github.com/google/uuid v1.3.1 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/luismorandi/my_first_go/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+
+}
